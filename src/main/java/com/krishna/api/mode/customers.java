18e23599c80b6f25f54650edc7495a3a960a4dd9
Krishna Thapa <krishna.nature91@gmail.com>
2018-10-17 02:37:59 +0100

    Added Schema and Data Sql files that are used in backed as MqSQL

---
+++ src/main/java/com/krishna/api/mode/customers.java
@@ -0,0 +1,23 @@
+package com.krishna.api.mode;
+
+import lombok.Data;
+
+@Data
+public class customers {
+
+    private Integer customerNumber;
+    private String customerName;
+    private String contactLastName;
+    private String contactFirstName;
+    private String phone;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String country;
+    private Integer salesRepEmployeeNumber;
+    private Double creditLimit;
+
+
+}
